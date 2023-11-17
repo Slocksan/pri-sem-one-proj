d8265f9735ea0698a692d7c3cc2df0ca1d52aecf
Vladimir Ponomarev <977005@gmail.com>
2023-11-18 04:02:43 +0500

    ДОбавил докер

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,26 @@
 import streamlit as st
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-ru-en")
+@st.cache_resource
+def load_tokenizer():
+    return AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-ru-en")
 
-model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-ru-en")
+@st.cache_resource
+def load_model():
+    return AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-ru-en")
 
-stringToTranslate = st.text_input("Введите ваш текст")
+stringToTranslate = st.text_input("Введите ваш текст (на русском)")
+
+st.image('fbzr.jpg')
 
 if (st.button("Перевести")):
-    input_ids = tokenizer.encode(stringToTranslate, return_tensors="pt")
+    input_ids = load_tokenizer().encode(stringToTranslate, return_tensors="pt")
 
-    outputs = model.generate(input_ids)
+    outputs = load_model().generate(input_ids)
 
-    decoded = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    decoded = load_tokenizer().decode(outputs[0], skip_special_tokens=True)
+
+    with open('queryis.txt', 'a', encoding='utf-8') as f:
+        f.write(stringToTranslate +" --- " + decoded + '\n')
+
     st.write("Перевод: " + decoded)
